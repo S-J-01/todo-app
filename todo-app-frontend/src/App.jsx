8f60dfeac06df40a781f076f80fce9d2c71245d6
Shreyas Jaltare <shreyasjaltare@gmail.com>
2024-04-22 19:39:58 +0530

    Added logic for getting input from react frontend

--- todo-app-frontend/src/App.jsx
+++ todo-app-frontend/src/App.jsx
@@ -1,33 +1,42 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from 'react'
+import axios from 'axios'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const [title,setTitle]=useState("")
+  const [description,setDescription]=useState("")
+  const [todos,setTodos]=useState([])
+
+  const handleTitleChange = (event)=>{
+   setTitle(event.target.value)
+  }
+
+  const handleDescriptionChange=(event)=>{
+    setDescription(event.target.value)
+  }
+
+  const onSubmit = ()=>{
+    axios.post('http://localhost:3000/todos',{
+      title:title,
+      description:description
+    }).then((response)=>{
+      console.log(response.data)
+    }).catch((err)=>{
+      console.log(err)
+    })
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      Title:
+      <input type="text" value={title} onChange={handleTitleChange} />
+      &nbsp;
+      Description:
+      <input type="text" value={description} onChange={handleDescriptionChange}  />
+      &nbsp;
+      <input type="button" value="Submit" onClick={onSubmit} />
+
     </>
   )
 }
